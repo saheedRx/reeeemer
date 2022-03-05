29c8b74ea82b574ad8064d1c563319d45b09bbc3
saheedRx <89293737+saheedRx@users.noreply.github.com>
2022-03-05 15:50:42 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -84,7 +84,7 @@ async def start_me(bot, update):
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
             if user.status == "kicked":
-               await update.reply_text(" Sorry,You've Been Flooding Me So My Owner Removed You From Using Me If You Think It's An Error Contact : @Faris_TG")
+               await update.reply_text(" Sorry,You've Been Flooding Me So My Owner Removed You From Using Me If You Think It's An Error Contact : @SAHEEDS_BOTS")
                return
         except UserNotParticipant:
             await update.reply_text(
@@ -102,12 +102,11 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/SAHEEDS_BOTS'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/saheed_rx')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Bots', url='https://t.me/SAHEEDS_BOTS')
                 ]
             ]
         ),
